55e73f9d3c6fae27d5d38f593efdea266e1f23bc
BimanLakhey <biman.lakhey@finnovetech.com>
2024-02-15 16:05:45 +0545

    added new case for sensitive data error

--- suyatra/lib/core/firebase_error_messages.dart
+++ suyatra/lib/core/firebase_error_messages.dart
@@ -1,17 +1,20 @@
 class Errors {
    static String show(String errorCode) {
-     switch (errorCode) {
-       case 'email-already-in-use':
-         return "This e-mail address is already in use, please use a different e-mail address.";
+      switch (errorCode) {
+        case 'email-already-in-use':
+          return "This e-mail address is already in use, please use a different e-mail address.";
 
-       case 'invalid-email':
-         return "The email address is badly formatted.";
+        case 'invalid-email':
+          return "The email address is badly formatted.";
 
-       case 'account-exists-with-different-credential':
-         return "The e-mail address in your Google account has been registered in the system before. Please login by trying other methods with this e-mail address.";
+        case 'account-exists-with-different-credential':
+          return "The e-mail address in your Google account has been registered in the system before. Please login by trying other methods with this e-mail address.";
 
-       case 'wrong-password' || 'invalid-credential':
-         return "E-mail address or password is incorrect.";
+        case 'wrong-password' || 'invalid-credential':
+          return "E-mail address or password is incorrect.";
+
+        case 'requires-recent-login':
+          return "Please login again to continue";
 
        default:
          return "An error has occurred";
