b03040edcce36a28a8ef5d6fbe29c8ea2e3d31d7
BimanLakhey <biman.lakhey@finnovetech.com>
2024-03-26 10:47:17 +0545

    added new app colors

--- suyatra/lib/constants/app_colors.dart
+++ suyatra/lib/constants/app_colors.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
-const primaryColor = Color(0xFFF9E3BC);
-const primaryDark = Color(0xFFBF6D0F);
+//orange colors
+const orange10 = Color(0xFFF9E3BC);
+const orange = Color(0xFFBF6D0F);
 
 const blackColor = Color(0xFF000000);
 const whiteColor = Color(0xFFFFFFFF);
 
 //grey colors
+const lightGrey = Color(0xFF868686);
+const grey10 = Color(0xFFE8E8E8);
+const grey20 = Color(0xFFDDDDDD);
+const grey40 = Color(0xFFA6A6A6);
 const grey50 = Color(0xFFFAFAFA);
+const grey70 = Color(0xFF525252);
+const grey90 = Color(0xFF262626);
 const grey100 = Color(0xFFF4F4F5);
 const grey200 = Color(0xFFE4E4E7);
 const grey300 = Color(0xFFD4D4D8);
@@ -17,3 +24,10 @@ const grey600 = Color(0xFF52525B);
 const grey700 = Color(0xFF3F3F46);
 const grey800 = Color(0xFF27272A);
 const grey900 = Color(0xFF18181B);
+
+//blue colors
+const primaryColor = Color(0xFFF1F8FF);
+const himalayanBlue = Color(0xFF77B5FE);
+const himalayanNight = Color(0xFF1A1A1A);
+
+const sunsetRed = Color(0xFFFF6F61);
