ee4725f571f724bb0688336d1c8addda01d8a517
BimanLakhey <biman.lakhey@finnovetech.com>
2024-03-26 16:50:27 +0545

    added google auth and shared preference service as singleton

--- suyatra/lib/core/service_locator.dart
+++ suyatra/lib/core/service_locator.dart
@@ -1,6 +1,8 @@
 import 'package:get_it/get_it.dart';
 import 'package:suyatra/features/authentication/presentation/auth_di.dart';
 import 'package:suyatra/services/firebase_service.dart';
+import 'package:suyatra/services/google_auth_service.dart';
+import 'package:suyatra/services/shared_preference_service.dart';
 
 import '../features/articles/presentation/article_di.dart';
 import '../services/api_service/api_base_helper.dart';
@@ -15,6 +17,10 @@ void setupLocator() {
 
   locator.registerLazySingleton(() => FirebaseService());
 
+  locator.registerLazySingleton(() => GoogleAuthService());
+
+  locator.registerLazySingleton(() => SharedPreferencesService());
+  
   articleDI();
   authDI();
 }
