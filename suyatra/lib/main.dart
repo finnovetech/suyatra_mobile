c736c197baba3d9bc97536c48772ee7787cf1a89
BimanLakhey <biman.lakhey@finnovetech.com>
2024-03-26 16:53:35 +0545

    initialized get user details use case

--- suyatra/lib/main.dart
+++ suyatra/lib/main.dart
@@ -50,6 +50,7 @@ class MyApp extends StatelessWidget {
             sendResetVerificationOTPUseCase: locator(),
             verifyResetOTPUseCase: locator(),
             createPasswordUseCase: locator(),
+            getUserDetailsUseCase: locator(),
           )
         ),
         BlocProvider(create: (context) => ActivityCubit()),
